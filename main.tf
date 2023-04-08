c49d8f0e1850ac99eda5eae38f53e22f792d57af
cozybear-dev <mjvogel99@gmail.com>
2023-04-08 21:17:55 +0200

    update dns records

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@
 resource "njalla_record_a" "tor_nodes" {
   for_each = {
     node1    = "23.137.249.8"
-    node2    = "79.137.202.181"
+    node2    = "77.91.85.147"
     node3    = "23.184.48.127"
     node4    = "23.184.48.128"
     node5    = "23.137.249.143"
     node6    = "23.137.249.185"
     node7    = "79.137.202.92"
-    node8    = "79.137.203.30"
+    node8    = "77.91.86.95"
     node9    = "185.17.0.40"
     node10   = "185.17.0.55"
     node11   = "89.185.85.140"
-    node12   = "79.137.207.137"
+    node12   = "77.91.78.210"
   }
   domain  = "shadowbrokers.eu"
   name    = "tor.${each.key}"
